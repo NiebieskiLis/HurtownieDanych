c8f4b59cc126ed3c20d3e35be279421c7262173f
NiebieskiLis <40572595+NiebieskiLis@users.noreply.github.com>
2019-05-13 17:05:01 +0200

    Dodano  SCD

--- Hotel.sql
+++ Hotel.sql
@@ -7,17 +7,31 @@ Merge Into [Travel Agency].dbo.HOTEL_DIM as TargetTable
 					Values ( SourceTable.[HotelID]
 					            , SourceTable.[HotelName]
 					            , SourceTable.[HotelType],GETDATE(), cast(NULL as date) )
-	--		When Matched 
-	--		         AND (SourceTable.[HotelName] <> TargetTable.[HotelName] 
-	--		        OR SourceTable.[HotelType] <> TargetTable.[HotelType] ) 
-	--		Then 
-	--				UPDATE -- change the data in the Target table (DimCustomers)
-	--				SET SourceTable.[ExpiredDate] = GETDATE()
-	--				 -- The ID in the Source is not found the Target
-	--				INSERT 
-	--				Values ( SourceTable.[HotelID]
-	--				            , SourceTable.[HotelName]
-	--				            , SourceTable.[HotelType],GETDATE(), cast(NULL as date) )
-			
-	;
+			When Matched
+			         AND (  SourceTable.[HotelName] <> TargetTable.[HotelName]
+			         OR  SourceTable.[HotelType] <> TargetTable.[HotelType] ) 
+					 AND TargetTable.[ExpiredDate] is null
+			Then 
+					UPDATE
+					SET TargetTable.[ExpiredDate] = GETDATE()
+			When Not Matched By Source 
+				Then 
+					UPDATE
+					SET TargetTable.[ExpiredDate] = GetDate()  ;
+go
 
+Insert into [Travel Agency].dbo.HOTEL_DIM
+		([HotelKEY]
+		, [HotelName]
+		, [HotelType],[InsertionDate] ,[ExpiredDate] )
+		Select HotelID, HotelName, HotelType ,GetDate(), cast(null as date)  from [Baza firmy].dbo.HOTEL 
+		Except 
+		Select HotelKEY, HotelName, HotelType,GetDate(), Null from [Travel Agency].dbo.HOTEL_DIM
+
+Exec seeTables;
+--select getdate()
+--update [Baza firmy].dbo.HOTEL
+--	set HotelName = 'HotelTest'
+--	where HotelID = 1
+--select * from HOTEL_DIM
+--where HotelKEY=1
